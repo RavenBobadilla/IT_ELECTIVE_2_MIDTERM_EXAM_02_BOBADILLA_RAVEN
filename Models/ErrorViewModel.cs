44248549969efc46b1aa23a7e1101a470e0baae7
Raven Bobadilla <bobadillaraymond04@gmail.com>
2026-08-08 11:15:17 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IT_ELECTIVE_2_MIDTERM_EXAM_02_BOBADILLA_RAVEN.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
